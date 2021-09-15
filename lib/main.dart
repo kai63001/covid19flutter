69d1885418575a47b6946b26f722b3ca2caa2b36
62021506 <62021506@up.ac.th>
2021-09-15 14:50:44 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,35 @@
+// ignore_for_file: prefer_const_constructors_in_immutables, prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  runApp(const MyApp());
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: Colors.white,
+    statusBarIconBrightness: Brightness.dark,
+  ));
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'COVID',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
+  MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -32,9 +39,67 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return const Scaffold(
+    return Scaffold(
+      backgroundColor: Colors.white,
       body: SafeArea(
-        child: Text("romeo"),
+        child: Stack(
+          children: [
+            // Box Main
+            Positioned(
+                top: size.height * 0.05,
+                width: size.width,
+                child: Center(
+                  child: Container(
+                    height: 130,
+                    width: size.width * 0.8,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.05),
+                          spreadRadius: 2,
+                          blurRadius: 5,
+                          offset: Offset(0, 5), // changes position of shadow
+                        ),
+                      ],
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(18.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Image.asset('assets/images/virus.png'),
+                          Column(
+                            children: [
+                              Text(
+                                '500',
+                                style: GoogleFonts.fredokaOne(
+                                  textStyle: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 40,
+                                      letterSpacing: .5),
+                                ),
+                              ),
+                              Text(
+                                'TODAY',
+                                style: GoogleFonts.fredokaOne(
+                                  textStyle: TextStyle(
+                                      color: Colors.grey[400],
+                                      fontSize: 20,
+                                      letterSpacing: .5),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                )),
+            
+          ],
+        ),
       ),
     );
   }
