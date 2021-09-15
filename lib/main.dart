1efd5aa0a9701ee943e31d4a0e9dfa4678b49f34
62021506 <62021506@up.ac.th>
2021-09-15 16:31:26 +0700

    update api

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: prefer_const_constructors_in_immutables, prefer_const_constructors
 
+import 'dart:convert';
+
 import 'package:covid/ui/case.dart';
 import 'package:covid/ui/country.dart';
 import 'package:covid/ui/death.dart';
@@ -7,7 +9,7 @@ import 'package:covid/ui/go_home.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:intl/intl.dart' as intl;
+import 'package:http/http.dart' as http;
 
 void main() {
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
@@ -41,6 +43,32 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  bool today = true;
+
+  void changeType(type) {
+    setState(() {
+      today = type;
+    });
+  }
+
+  late Map<String, dynamic> covidData;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchCovidData();
+  }
+
+  Future fetchCovidData() async {
+    var response = await http.get(
+        Uri.parse("https://static.easysunday.com/covid-19/getTodayCases.json"));
+    if (response.statusCode == 200) {
+      setState(() {
+        covidData = jsonDecode(response.body);
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -54,12 +82,7 @@ class _MyHomePageState extends State<MyHomePage> {
               SizedBox(
                 height: 20,
               ),
-              Container(
-                decoration: BoxDecoration(
-                  color: Colors.white
-                ),
-                child: Text("romeo"),
-              ),
+              selectionCase(size),
               Case(size: size),
               Country(),
               SizedBox(
@@ -76,6 +99,69 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ],
           ),
+        ),
+      ),
+    );
+  }
+
+  Container selectionCase(Size size) {
+    return Container(
+      width: size.width * 0.8,
+      height: 50,
+      decoration: BoxDecoration(
+          border: Border.all(width: 2.0, color: Colors.black),
+          borderRadius: BorderRadius.all(Radius.circular(3))),
+      child: Center(
+        child: Stack(
+          children: [
+            AnimatedPositioned(
+              // left: 7,
+              left: today ? 7 : size.width * 0.42,
+              top: 7,
+              duration: Duration(milliseconds: 200),
+              child: Container(
+                height: 30,
+                width: size.width * 0.35,
+                decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.all(Radius.circular(3))),
+              ),
+            ),
+            Row(
+              children: [
+                Expanded(
+                    child: GestureDetector(
+                  onTap: () => {changeType(true)},
+                  child: Center(
+                    child: Text(
+                      'TODAY',
+                      style: GoogleFonts.fredokaOne(
+                        textStyle: TextStyle(
+                            color: today ? Colors.white : Colors.black,
+                            fontSize: 15,
+                            letterSpacing: .5),
+                      ),
+                    ),
+                  ),
+                )),
+                Expanded(
+                    child: GestureDetector(
+                  onTap: () => {changeType(false)},
+                  child: Center(
+                    child: Text(
+                      'TOTAL',
+                      style: GoogleFonts.fredokaOne(
+                        textStyle: TextStyle(
+                            color: !today ? Colors.white : Colors.black,
+                            fontSize: 15,
+                            letterSpacing: .5),
+                      ),
+                    ),
+                  ),
+                )),
+              ],
+            ),
+          ],
         ),
       ),
     );
